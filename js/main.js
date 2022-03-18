7398a1fd5587b31bf145ead4ffa7001e7a1f0bb2
SimoneScapin <simonescap87@gmail.com>
2022-03-18 17:19:34 +0100

    aggiunto array e submit mode per il tasto enter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const app = new Vue({
     el: '#app',
     data :{
-    title:  'Todo App',
-    
-    }
+    title: 'Todo App',
+    newTodo: '',
+    todoData: [],
+
+    },
+    methods: {
+        addTodo(){
+            this.todoData.push({
+                title: this.newTodo,
+                done:false,
+            });     
+        
+        }
+    },
 })
